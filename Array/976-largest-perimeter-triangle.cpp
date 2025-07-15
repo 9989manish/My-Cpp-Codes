0576cd46d74a05e9ee2b945fbb2ed0c489d1c96e
9989manish <manishbaraiya8654@gmail.com>
2025-07-15 21:59:39 +0530

    Updated triangle solution, removed duplicate file, added max depth of parentheses

--- Array/976-largest-perimeter-triangle.cpp
+++ Array/976-largest-perimeter-triangle.cpp
@@ -1,11 +1,14 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int largestPerimeter(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-       int max_pari=0;
+    int largestPerimeter(std::vector<int>& nums) {
+        std::sort(nums.begin(),nums.end());
+        int max_pari=0;
         for(int i=0;i<=nums.size()-3;i++){
             if(nums[i]+nums[i+1]>nums[i+2]){
-                max_pari=max(max_pari,nums[i]+nums[i+1]+nums[i+2]);
+                max_pari=std::max(max_pari,nums[i]+nums[i+1]+nums[i+2]);
             }
         }
         if(max_pari)return max_pari;
